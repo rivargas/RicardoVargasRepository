bd6c8f14e55cf7f72dc0e9a9dcadf2a26e1525f9
Ricardo Vargas <ricardovargasrv94@gmail.com>
2020-09-10 11:34:46 -0700

    This i working for smaller viewports.
    Still has to be fixed up more

---
+++ js/index.js
@@ -0,0 +1,40 @@
+function navBarButtonClick(){
+      const navbar = document.getElementById("navbar");
+      const navbarToggle = navbar.querySelector(".navbar-toggle");
+
+      openMobileNavbar();
+      navbarToggle.addEventListener("click", () => {
+            closeMobileNavbar();
+      });
+
+      function openMobileNavbar() {
+      navbar.classList.add("opened");
+      navbarToggle.setAttribute("aria-label", "Close navigation menu.");
+      }
+
+      function closeMobileNavbar() {
+      navbar.classList.remove("opened");
+      navbarToggle.setAttribute("aria-label", "Open navigation menu.");
+      }
+
+
+      /*
+      navbarToggle.addEventListener("click", () => {
+      if (navbar.classList.contains("opened")) {
+      closeMobileNavbar();
+      } else {
+      openMobileNavbar();
+      }
+      });
+      */
+
+      const navbarMenu = navbar.querySelector(".navbar-menu");
+      const navbarLinksContainer = navbar.querySelector(".navbar-links");
+
+      navbarLinksContainer.addEventListener("click", (clickEvent) => {
+      clickEvent.stopPropagation();
+      });
+
+      navbarMenu.addEventListener("click", closeMobileNavbar);
+
+}
